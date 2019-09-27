21b4778113c82e1dc1e0d640a914bb5674ecc24d
tweakydragon@gmail.com <tweakydragon@gmail.com>
2019-09-27 15:51:46 -0400

    adding page

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/hello', function(req, res) {
+    res.sendFile(__dirname + '/hello.html');
+});
+
 app.listen(7000, function() {
     console.log('listening on port 3000');
 });
